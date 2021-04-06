085e5c46486edde4ea466a5908de44e7d7e39180
Matthew <mwbhtx@outlook.com>
2021-04-06 15:26:33 -0500

    Added Get Functions For Register Value / Index
    * Added function to get register value from index argument
    * Added function to get register index from string argument

--- Main.cs
+++ Main.cs
@@ -8,14 +8,43 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+enum reg_Index: int {
+
+    i_regZero = 0,
+    i_regAt, 
+    i_regV0,
+    i_regV1,
+    i_regA0,
+    i_regA1,
+    i_regA2,
+    i_regA3,
+    i_regT0,
+    i_regT1,
+    i_regT2,
+    i_regT3,
+    i_reg_NumberOfRegisters
+
+}
+
 namespace MIPS_Instruction_Analyzer
 {
+
+
+
     public partial class Main : Form
     {
+
+
+
+        /* Initialize Register Array : Values Initialize To Zero */
+        int[] registerArray = new int[(int)reg_Index.i_reg_NumberOfRegisters];
+
 
         public Main()
         {
             InitializeComponent();
+
+
         }
 
         private void Main_Load(object sender, EventArgs e)
@@ -25,27 +54,94 @@ namespace MIPS_Instruction_Analyzer
 
         private void button2_Click(object sender, EventArgs e)
         {
-            var instructString = instructBox.Text;
-            instructString.Trim();
-
-            var instructArray = instructString.Split(' ');
-
-            //
+         
 
 
-            for (int x = 0; x < instructArray.Length; x++ )
-            {
+        }
 
-                if (instructArray[x] == "add")
-                {
-                    Console.WriteLine(instructArray[x]);
-                }
+
+
+        /* Get Current Register Value From Register Array */
+        private int getRegisterValue(int registerIndex)
+        {
+            int registerValue = 0;
+
+            if (registerIndex >= 0 && registerIndex < (int) reg_Index.i_reg_NumberOfRegisters) {
+
+                registerValue = registerArray[registerIndex]; 
+
             }
 
+            return registerValue; 
+        }
 
 
+        /* Get Register Array Index From String Represented Register Name */
+        private int getRegisterIndexFromString(string registerAsString)
+        {
+            // Init Register Index Return Value
+            int registerIndex = 0;
 
 
+            // Compare Register As String Argument
+            switch (registerAsString)
+            {
+                case "$zero":
+
+                    registerIndex = (int)reg_Index.i_regZero;
+                    break;
+                case "$at":
+
+                    registerIndex = (int)reg_Index.i_regAt;
+                    break;
+                case "$v0":
+
+                    registerIndex = (int)reg_Index.i_regV0;
+                    break;
+                case "$v1":
+
+                    registerIndex = (int)reg_Index.i_regV1;
+                    break;
+                case "$a0":
+
+                    registerIndex = (int)reg_Index.i_regA0;
+                    break;
+                case "$a1":
+
+                    registerIndex = (int)reg_Index.i_regA1;
+                    break;
+                case "$a2":
+
+                    registerIndex = (int)reg_Index.i_regA2;
+                    break;
+                case "$a3":
+
+                    registerIndex = (int)reg_Index.i_regA3;
+                    break;
+                case "$t0":
+
+                    registerIndex = (int)reg_Index.i_regT0;
+                    break;
+                case "$t1":
+
+                    registerIndex = (int)reg_Index.i_regT1;
+                    break;
+                case "$t2":
+
+                    registerIndex = (int)reg_Index.i_regT2;
+                    break;
+                case "$t3":
+
+                    registerIndex = (int)reg_Index.i_regT3;
+                    break;
+                default:
+                    registerIndex = 0xFF;
+                    break;
+
+            }
+
+            // On Return, Check If Values Is 0xFF. A returned value of 0xFF represents no string match. 
+            return registerIndex;
 
         }
     }
